8e34364b848755cb764262b73c9596cb28cdaede
AdhithyanM <adhithyanmanoharan26@gmail.com>
2024-03-24 22:33:01 +0530

    Initial Commit

---
+++ setup/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo *******************************************************************
+echo Starting the replica set
+echo *******************************************************************
+
+# wait for a while until the 3 mongo containers are fully up
+sleep 10 | echo Sleeping
+
+# connect to the first container and execute the replica setup script
+mongosh "mongodb://mongo_primary:27017" --file replicaSet.js
